16c6d3162c910156e63b5337b333f653d191b8f0
Jhonathan Salas <jhonssegura@outlook.com>
2021-10-28 19:53:22 -0500

    Primeros avances de la estructura y funcionalidad

---
+++ generator/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+import random
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def password(request):
+
+    characters = list('abcdefghijklmnopqrstuvwxyz')
+    generated_password = ''
+
+    length = int(request.GET.get('length'))
+
+    for x in range(length):
+        generated_password += random.choice(characters)
+
+    return render(request, 'password.html', {'password': generated_password})
